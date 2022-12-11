9e2677ef98af459f017f0b7d20ae385f7827c32a
Jerry Wang <jerryappleid761208@gmail.com>
2022-12-11 14:08:09 -0800

    docker

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT ?? 4000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('The server is working!');
